c12d022cb70a38d23d00a44f4b47fc8502fff83f
Mohammadshahsamirbhu <samir_reaz@yahoo.com>
2022-02-01 15:03:54 +0600

    5 model data uploaded

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -13,9 +13,31 @@ class ItemModel {
 }
 
 List<ItemModel> items = [
-  ItemModel(itemName: 'h1', itemCata: 'front_hand', itemImage: ''),
-  ItemModel(itemName: 'h1', itemCata: 'back_hand', itemImage: ''),
-  ItemModel(itemName: 'h1', itemCata: 'front_hand', itemImage: ''),
-  ItemModel(itemName: 'h1', itemCata: 'back_hand', itemImage: ''),
-  ItemModel(itemName: 'h1', itemCata: 'front_hand', itemImage: ''),
+  ItemModel(
+    itemName: 'h1',
+    itemCata: 'front_hand',
+    itemImage: 'https://i.ytimg.com/vi/w6uG6Ac1RPs/maxresdefault.jpg',
+  ),
+  ItemModel(
+    itemName: 'h1',
+    itemCata: 'back_hand',
+    itemImage: 'https://i.ytimg.com/vi/8XhwkaqxWOI/maxresdefault.jpg',
+  ),
+  ItemModel(
+    itemName: 'h1',
+    itemCata: 'front_hand',
+    itemImage: 'https://i.ytimg.com/vi/n2MrvnCk-ls/maxresdefault.jpg',
+  ),
+  ItemModel(
+    itemName: 'h1',
+    itemCata: 'back_hand',
+    itemImage:
+        'https://i0.wp.com/shaadiwish.com/blog/wp-content/uploads/2020/04/arabic-mehandi-836x1024.jpg',
+  ),
+  ItemModel(
+    itemName: 'h1',
+    itemCata: 'front_hand',
+    itemImage:
+        'https://crayon.pk/wp-content/uploads/2018/11/Wonderful-front-Hand-Mehndi-Design.jpg',
+  ),
 ];
